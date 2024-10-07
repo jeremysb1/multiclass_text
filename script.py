d3f999e363f5e9a0445b692273f8b17e978a5fde
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2024-10-07 10:27:43 -0500

    adding python script file

---
+++ script.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from transformers import DistilBertTokenizer, DistilBertModel
+from tqdm import tqdm
+import argparse
+import os
+import pandas as pd
+
+s3_path = 's3://hf-mc-tcoh-bucket/training_data/newsCorpora.csv'
+df = pd.read_csv(s3_path, sep='\t', names=['ID', 'TITLE', 'URL', 'PUBLISHER', 'CATEGORY', 'STORY', 'HOSTNAME', 'TIMESTAMP'])
+
+df = df[['TITLE', 'CATEGORY']]
