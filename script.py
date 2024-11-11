842438ad5fbdf64449a3b47be129e53647bed1ac
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2024-11-11 09:56:13 -0600

    added forward method to DistilBertClass

--- script.py
+++ script.py
@@ -118,3 +118,21 @@ class DistilBertClass(torch.nn.Module):
         self.dropout = torch.nn.Dropout(0.3)
         
         self.classifier = torch.nn.Linear(768,4)
+    
+    def forward(self, input_ids, attention_mask):
+        
+        output_1 = self.l1(input_ids=input_ids, attention_mask=attention_mask)
+        
+        hidden_state = output_1[0]
+        
+        pooler = hidden_state[:,0]
+        
+        pooler = self.pre_classifier(pooler)
+        
+        pooler = torch.nn.ReLU()(pooler)
+        
+        pooler = self.dropout(pooler)
+        
+        output = self.classifier(pooler)
+        
+        return output
